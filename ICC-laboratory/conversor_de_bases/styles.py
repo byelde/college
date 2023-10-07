5bf388348091dd61fd8f13f94c5249ea177e1248
Gabryel Adriano Borges de Souza <byelacademico@gmail.com>
2023-10-07 20:48:31 -0300

    test file

--- ICC-laboratory/conversor_de_bases/styles.py
+++ ICC-laboratory/conversor_de_bases/styles.py
@@ -1,16 +1,32 @@
-class Styles:
-    # def __init__(self):
-        header = {
-            'background-color': '#E72313',
-            'width':'100%',
-            'height':'10%'
-        }
+na10 = 0
+num = 765
+num = str(num)[::-1]
+bi=8
+bf=16
 
-        headerText = {
-            'color': '#FFF',
-            'font-family': 'Kantumruy',
-            'font-size': '32px',
-            'font-style': 'normal',
-            'font-weight': '700',
-            'line-height': 'normal',
-        }
+db = '0123456789abcdefghijklmnopqrstuvwxyz'
+
+for idx_digito, digito in enumerate(num):
+    for idx_db, db_digito in enumerate(db):
+        if digito == db_digito:
+            na10 += ((int(bi)**idx_digito)*int(idx_db))
+
+nabf = ''
+
+while True:
+         
+        if na10 < bf:
+            for idx_db, db_digito in enumerate(db):
+                  if str(na10) == str(idx_db):
+                       nabf += db_digito
+                       break
+            break
+
+        
+        for idx_db, db_digito in enumerate(db):
+            if idx_db == na10%bf:
+                nabf += str(db_digito)
+                na10 = na10//bf
+                break
+
+
